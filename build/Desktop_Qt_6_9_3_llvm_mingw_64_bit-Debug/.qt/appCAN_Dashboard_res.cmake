6afeddddf2c12e5f1ff3e4c84ef1478690c7525c
Vinh Le <khongphaidangvuadau794@gmail.com>
2026-01-11 10:17:02 +0700

    tui moi ngu day

---
+++ build/Desktop_Qt_6_9_3_llvm_mingw_64_bit-Debug/.qt/appCAN_Dashboard_res.cmake
@@ -0,0 +1,9 @@
+
+set(target "appCAN_Dashboard")
+set(working_dir "C:/Project/CAN_Dashboard")
+set(src_and_dest_list
+    "C:/Project/CAN_Dashboard/asserts_resource.qrc"
+    "C:/Project/CAN_Dashboard/build/Desktop_Qt_6_9_3_llvm_mingw_64_bit-Debug/CAN_Dashboard/asserts_resource.qrc"
+
+)
+set(timestamp_file "C:/Project/CAN_Dashboard/build/Desktop_Qt_6_9_3_llvm_mingw_64_bit-Debug/.qt/appCAN_Dashboard_res.txt")
